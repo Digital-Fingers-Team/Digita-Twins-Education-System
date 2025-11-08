762b5951e8e03865808cdb6c6841c0177f5b81de
Digital-Fingers-Team <198120516+Digital-Fingers-Team@users.noreply.github.com>
2025-11-08 09:53:59 -0500

    manus

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { NotificationsGateway } from './notifications/notifications.gateway';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, NotificationsGateway],
 })
 export class AppModule {}
